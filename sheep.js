711a32cfa54b269592e654810b566dc3da03126e
Alejando VF <worshipingsheep.3@gmail.com>
2024-12-19 02:59:04 -0800

    Update sheep.js

--- sheep.js
+++ sheep.js
@@ -1,104 +1,103 @@
 class Sheep {
   constructor(size, x, y) {
-      this.size = size;
-      this.posX = x;
-      this.posY = y;
-      this.isDragging = false;
-      this.dragStartX = 0;
-      this.dragStartY = 0;
+    this.size = size;
+    this.posX = x;
+    this.posY = y;
+    this.isDragging = false;
+    this.dragStartX = 0;
+    this.dragStartY = 0;
   }
   
   get getX() {
-      return this.posX;
+    return this.posX;
   }
   
   get getY() {
-      return this.posY;
+    return this.posY;
   }
   
   isWithinBounds(newX, newY) {
-      return (
-          newX >= 0 && 
-          newX + this.size <= window.innerWidth &&
-          newY >= 0 && 
-          newY + this.size <= window.innerHeight
-      );
+    return (
+      newX >= 0 && 
+      newX + this.size <= window.innerWidth &&
+      newY >= 0 && 
+      newY + this.size <= window.innerHeight
+    );
   }
   
   move(newX, newY) {
-      if (this.isWithinBounds(newX, newY)) {
-          this.posX = newX;
-          this.posY = newY;
-      }
+    if (this.isWithinBounds(newX, newY)) {
+      this.posX = newX;
+      this.posY = newY;
+    }
   }
   
   startDrag(mouseX, mouseY) {
-      this.isDragging = true;
-      this.dragStartX = mouseX - this.posX;
-      this.dragStartY = mouseY - this.posY;
+    this.isDragging = true;
+    this.dragStartX = mouseX - this.posX;
+    this.dragStartY = mouseY - this.posY;
   }
   
   endDrag() {
-      this.isDragging = false;
+    this.isDragging = false;
   }
   
   render(ctx) {
-      ctx.fillStyle = 'blue';
-      ctx.fillRect(this.posX, this.posY, this.size, this.size);
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(this.posX, this.posY, this.size, this.size);
   }
 
   isPointInside(x, y) {
-      return (
-          x > this.posX && 
-          x < this.posX + this.size &&
-          y > this.posY && 
-          y < this.posY + this.size
-      );
+    return (
+      x > this.posX && 
+      x < this.posX + this.size &&
+      y > this.posY && 
+      y < this.posY + this.size
+    );
   }
 }
 
 class SheepManager {
   constructor() {
-      this.sheep = [];
+    this.sheep = [];
   }
 
-        if (square.isDragging) {
   addSheep(size, x, y) {
-      const newSheep = new Sheep(size, x, y);
-      this.sheep.push(newSheep);
-      return newSheep;
+    const newSheep = new Sheep(size, x, y);
+    this.sheep.push(newSheep);
+    return newSheep;
   }
 
   handleMouseDown(mouseX, mouseY) {
-      // Check sheep in reverse order so we select the top-most sheep first
-      for (let i = this.sheep.length - 1; i >= 0; i--) {
-          const sheep = this.sheep[i];
-          if (sheep.isPointInside(mouseX, mouseY)) {
-              sheep.startDrag(mouseX, mouseY);
-              // Move the selected sheep to the end of the array (top of render stack)
-              this.sheep.splice(i, 1);
-              this.sheep.push(sheep);
-              break;
-          }
+    // Check sheep in reverse order so we select the top-most sheep first
+    for (let i = this.sheep.length - 1; i >= 0; i--) {
+      const sheep = this.sheep[i];
+      if (sheep.isPointInside(mouseX, mouseY)) {
+        sheep.startDrag(mouseX, mouseY);
+        // Move the selected sheep to the end of the array (top of render stack)
+        this.sheep.splice(i, 1);
+        this.sheep.push(sheep);
+        break;
       }
+    }
   }
 
   handleMouseMove(mouseX, mouseY) {
-      this.sheep.forEach(sheep => {
-          if (sheep.isDragging) {
-              const newX = mouseX - sheep.dragStartX;
-              const newY = mouseY - sheep.dragStartY;
-              sheep.move(newX, newY);
-          }
-      });
+    this.sheep.forEach(sheep => {
+      if (sheep.isDragging) {
+        const newX = mouseX - sheep.dragStartX;
+        const newY = mouseY - sheep.dragStartY;
+        sheep.move(newX, newY);
+      }
+    });
   }
 
   handleMouseUp() {
-      this.sheep.forEach(sheep => sheep.endDrag());
+    this.sheep.forEach(sheep => sheep.endDrag());
   }
 
   renderAll(ctx) {
-      this.sheep.forEach(sheep => sheep.render(ctx));
+    this.sheep.forEach(sheep => sheep.render(ctx));
   }
 }
 
@@ -119,26 +118,26 @@ document.addEventListener('DOMContentLoaded', function() {
   
   // Draw function
   function draw() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      sheepManager.renderAll(ctx);
-      requestAnimationFrame(draw);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    sheepManager.renderAll(ctx);
+    requestAnimationFrame(draw);
   }
   
   // Mouse event handlers
   canvas.addEventListener('mousedown', (e) => {
-      const mouseX = e.clientX - canvas.offsetLeft;
-      const mouseY = e.clientY - canvas.offsetTop;
-      sheepManager.handleMouseDown(mouseX, mouseY);
+    const mouseX = e.clientX - canvas.offsetLeft;
+    const mouseY = e.clientY - canvas.offsetTop;
+    sheepManager.handleMouseDown(mouseX, mouseY);
   });
   
   canvas.addEventListener('mousemove', (e) => {
-      const mouseX = e.clientX - canvas.offsetLeft;
-      const mouseY = e.clientY - canvas.offsetTop;
-      sheepManager.handleMouseMove(mouseX, mouseY);
+    const mouseX = e.clientX - canvas.offsetLeft;
+    const mouseY = e.clientY - canvas.offsetTop;
+    sheepManager.handleMouseMove(mouseX, mouseY);
   });
   
   canvas.addEventListener('mouseup', () => {
-      sheepManager.handleMouseUp();
+    sheepManager.handleMouseUp();
   });
   
   // Initialize canvas size
@@ -149,12 +148,11 @@ document.addEventListener('DOMContentLoaded', function() {
   draw();
   
   window.addEventListener('resize', () => {
-      width = kiwiElement.offsetWidth;
-      height = kiwiElement.offsetHeight;
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
-      console.log('Width:', width);
-      console.log('Height:', height);
+    width = kiwiElement.offsetWidth;
+    height = kiwiElement.offsetHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    console.log('Width:', width);
+    console.log('Height:', height);
   });
 });
-
